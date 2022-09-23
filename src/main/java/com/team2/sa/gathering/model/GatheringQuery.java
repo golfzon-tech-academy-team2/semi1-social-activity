8c68820c28f027ed787e7fa3caae3b4e6703d372
yuyeon329 <peachberryjr@gmail.com>
2022-09-23 17:04:02 +0900

    resolved conflict

--- src/main/java/com/team2/sa/gathering/model/GatheringQuery.java
+++ src/main/java/com/team2/sa/gathering/model/GatheringQuery.java
@@ -12,6 +12,6 @@ public interface GatheringQuery {
 	String GATHERING_DELETE = "";
 	String GATHERING_SELECTALL = "select * from gathering where ispublic = 'T'  ORDER BY gnum ASC";
 	String GATHERING_SELECTONE = "";
-	String PUBGATHERING_JOIN = "insert into gatheringuserinfo values(?,?,(select unum from userinfo where id=?))";
+	String GATHERING_SEARCHLIST = "select * from gathering where minAge>=? and maxAge<=? and sex=? and ispublic='T' order by gnum desc";
 
 }
