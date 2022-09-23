297412238bc0d77002cb78ae0a4797301dedb5d1
yuyeon329 <peachberryjr@gmail.com>
2022-09-23 16:53:04 +0900

    modify join pubgathering

--- src/main/java/com/team2/sa/gathering/controller/InsertGatheringController.java
+++ src/main/java/com/team2/sa/gathering/controller/InsertGatheringController.java
@@ -40,7 +40,7 @@ public class InsertGatheringController extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
-	 */
+	 */ 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
